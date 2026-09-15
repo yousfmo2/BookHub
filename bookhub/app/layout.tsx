0a219f40dd8c80eb8d085f36b198301efbd5e8cb
Y0uss3f99 <youssefmo12you@gmail.com>
2026-09-15 20:53:09 +0300

    add Vercel

--- bookhub/app/layout.tsx
+++ bookhub/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./_components/_Navbar/page";
 import Footer from "./_components/_footer/page";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
